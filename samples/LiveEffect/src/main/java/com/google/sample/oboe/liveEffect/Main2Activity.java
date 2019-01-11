7361e3068b960b4f1e829b64e7a12e3fe9f21711
tamnuwat <tamnuwat.val@gmail.com>
2019-01-11 21:05:30 +0700

    version1

---
+++ samples/LiveEffect/src/main/java/com/google/sample/oboe/liveEffect/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.google.sample.oboe.liveEffect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+
+
+public class Main2Activity extends Activity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+
+
+}
